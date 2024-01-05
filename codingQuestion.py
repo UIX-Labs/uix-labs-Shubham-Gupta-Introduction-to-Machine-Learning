51491ea9da5ba09030efbc9d82e97cbaecf49bbe
Shubham Gupta <guptashubhamofficial24@gmail.com>
2024-01-05 17:15:00 +0530

    task: Task: Coding Question

--- codingQuestion.py
+++ codingQuestion.py
@@ -1,1 +1,12 @@
+L=int(input())
+N=int(input())
 
+for i in range(0,N):
+    W,H=map(int, input().split())
+   
+    if (W<L or H<L) and (1<=W<10000 and 1<=H<10000):
+        print("UPLOAD ANOTHER")
+    elif W==H :
+        print("ACCEPTED")
+    else :
+        print("CROP IT")
